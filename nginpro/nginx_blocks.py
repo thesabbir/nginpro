097c04ac0bb8eda23a575e687d9c6447aee651ad
Sabbir Ahmed <sabbiirr@gmail.com>
2016-11-02 19:05:57 +0600

    remove blocks file

--- nginpro/nginx_blocks.py
+++
@@ -1,9 +0,0 @@
-from string import Template
-
-
-def make_server_block(content):
-    return Template("server {\n ${content} \n}").safe_substitute(content=content)
-
-
-def make_location_block(content, pattern='/'):
-    return Template("location ${pattern} {\n ${content} \n}").safe_substitute(pattern=pattern, content=content)
