c9f7060d2ac75c6b52fa067c130bcdafcf02bbd6
Sabbir Ahmed <sabbiirr@gmail.com>
2016-11-02 15:50:57 +0600

    setup.py init

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='nginxpro',
+    version='0.0.1',
+    packages=['nginxpro'],
+    license='MIT',
+    long_description="nginx configuration tool",
+)
